205d9c5f2d75c2a9dca2d7676b0bd3e1ff236556
João Henrique Barbosa <joaohenriquebarbosa21@gmail.com>
2019-05-07 00:21:37 -0300

    first commit

---
+++ ghibli/README.md
@@ -0,0 +1,5 @@
+# Aplicativo Studio Ghibli
+
+- [Veja o tutorial](https://www.joaohenriquebarbosa.com.br/como-se-conectar-a-uma-api-com-javascript)
+
+- [View the demo](https://JoaoHenriqueBarbosa.github.io/sandbox/ghibli/)
---
+++ ghibli/scripts.js
@@ -0,0 +1,33 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  });
